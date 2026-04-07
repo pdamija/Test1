66fc973f945df3a0eb136f8f98110cbcc2676d76
pdamija <jdamija@gmail.com>
2026-04-07 16:36:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 
 # ── Page config ───────────────────────────────────────────────────────
 st.set_page_config(
